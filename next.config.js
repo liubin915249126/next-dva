12319b4e46dd3d82ddd37ca87e193ad75c1f3d60
liubin915249126 <915249126@qq.com>
2019-03-13 16:24:44 +0800

    add less loader

--- next.config.js
+++ next.config.js
@@ -1,29 +1,25 @@
 const withLess = require("@zeit/next-less");
 const withCss = require("@zeit/next-css");
-const withLessExcludeAntd = require("./next.less.config.js")
+const withLessExcludeAntd = require("./next-less.config.js")
 var path = require("path");
 
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = (file) => {}
+}
 
 module.exports = withLessExcludeAntd(
   // withCss(
-   {
+    {
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+    },
     lessLoaderOptions: {
       javascriptEnabled: true,
-      importLoaders: 1,
-      localIdentName: "[local]___[hash:base64:5]"
-    },
-    // distDir: "lwbBlogBuild",
-    cssModules: true,
-    webpack: function(config, env) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        components: path.resolve("./components"),
-        "~": path.resolve("./static")
-      };
-      // console.log(JSON.stringify(env))
-      // console.log(JSON.stringify(config))
-      return config;
+      // modifyVars: modifyVars
     }
+    // distDir: "lwbBlogBuild",
   }
   // )
 );
